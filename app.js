8ab776a8c4275cb7dca4f3e01aac1aab280dcb4b
INDRASISH BISWAS <indrasish.biswas2006@gmail.com>
2026-09-20 10:39:12 +0530

    DAY 10

--- app.js
+++ app.js
@@ -37,6 +37,6 @@ app.use('/history', historyRoute);
 
 const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running and listening on port ${PORT}`);
 });
